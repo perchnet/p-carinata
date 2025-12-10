b9286104e3d7cdf155d6088c9e01692d31c603a0
bri <284789+b-@users.noreply.github.com>
2025-12-09 21:38:05 -0500

    no remote runners

--- tf/backend.tf
+++ tf/backend.tf
@@ -3,8 +3,8 @@ terraform {
     hostname     = "otaco.app"
     organization = "org_01JZPXMHJHKPPAEN9ANR2T6AP1"
     workspaces {
-     # name = "0bc75457-0404-4b57-813c-4398223fd627"
-      name = "7bebeec9-82c6-4f9b-9ba3-adcc22815410"
+     name = "0bc75457-0404-4b57-813c-4398223fd627"
+     #name = "7bebeec9-82c6-4f9b-9ba3-adcc22815410"
 
     }
   }
